e5eebb27cc4579f22a828180064b20360600a717
Carlos Borges <carlosjbm11780@gmail.com>
2022-10-11 15:23:54 -0400

    Fetch con Async Away

--- js/ajax.js
+++ js/ajax.js
@@ -60,9 +60,39 @@
     .catch((err) => {
       console.log(err);
       let message = err.statusText || "Ocurrio un error";
-      $err.innerHTML = `Error ${err.status} ${message}`;
+      $fetch.innerHTML = `Error ${err.status} ${message}`;
     })
     .finally(() => {
       console.log("Este texto siempre va a mostrarse");
     });
+})();
+
+//Fetch con Async-Away
+(() => {
+  const $fetchAsync = document.getElementById("fetch-async"),
+    $err = document.getElementById("err"),
+    $fragment = document.createDocumentFragment();
+  //creamos la funcion asincrona
+  async function getData() {
+    try {
+      let res = await fetch("https://jsonplaceholder.typicode.com/users");
+      json = await res.json();
+
+      if (!res.ok) throw { status: res.status, statusText: res.statusText };
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name}--- (${el.email})--- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetchAsync.appendChild($fragment);
+    } catch (err) {
+      console.log(err);
+      let message = err.statusText || "Ocurrio un error";
+      $fetchAsync.innerHTML = `Error ${err.status} ${message}`;
+    } finally {
+      console.log("Este texto se mostrara de cualquier manera");
+    }
+  }
+
+  getData();
 })();
